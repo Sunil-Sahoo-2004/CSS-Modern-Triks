550cdd0b52d2964b815dbd98be0d4b347caeb282
Sunil Sahoo <sunilsahoo1525@gmail.com>
2024-10-06 13:36:35 +0530

    My First Commit

---
+++ loader-react/loader-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SampleLoader from "./SampleLoader"
+
+function App() {
+  return (
+    <div style={{height: "400px", width: "400px"}}>
+      <SampleLoader />
+    </div>
+  );
+}
+
+export default App
---
+++ loader-react/loader-app/src/SampleLoader.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SampleLoader = () => {
+    return <div className="boxes">
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+};
+export default SampleLoader;
